5a9a6c876bf3b2b30fadf87b16256d93264b88c0
Helberth A. D. Martins <127428020+Helberthadm@users.noreply.github.com>
2025-11-14 22:59:43 -0300

    Add files via upload

---
+++ src/Page/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+
+  return (
+
+    <View style={{flex:1, alignItems: 'center', justifyContent:'center'}}>
+
+      <Text> Puc Minas - Helberth </Text>
+
+    </View>
+  );
+}
+
+export default HomePage;
